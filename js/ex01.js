b29151b72827944e69c10b65def6b8a4011dc30e
yuvalsidis <yuvalsidis@gmail.com>
2024-02-14 16:14:49 +0200

    Modify ex01.js working as expected

--- js/ex01.js
+++ js/ex01.js
@@ -1,4 +1,17 @@
 'use strict'
 
+var input = ['return', 'phrases', 'with one word']
+var expected = ['return', 'phrases']
+var actual = onlyOneWord(input)
+actual[0] = 'return1'
 
 
+function onlyOneWord(input){
+    const result = input.filter((word) => !word.includes(' '))
+    return result
+}
+function wordInclude(word){
+    return word.includes(' ')
+}
+
+console.log('INPUT:', input, 'EXPECTED:', expected, 'ACTUAL:', actual);
