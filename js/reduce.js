b5106d64d7a17b887a1084175ef4de14f18a7297
yuvalsidis <yuvalsidis@gmail.com>
2024-02-14 19:08:50 +0200

    Link file ex reduce.js  and initilize array

---
+++ js/reduce.js
@@ -0,0 +1,16 @@
+const array1 = [1, 2, 3, 4];
+/*
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+var sum = 0
+var newSum = array1.forEach((num) => sum += num)
+console.log(sum);
+*/
+
