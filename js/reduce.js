f07435d3bad256188097ced0d426d7d32a1ee1b2
yuvalsidis <yuvalsidis@gmail.com>
2024-02-14 20:53:31 +0200

    Add deptCollectiveExp to reduce.js

--- js/reduce.js
+++ js/reduce.js
@@ -1,5 +1,7 @@
-const array1 = [1, 2, 3, 4];
+'use strict'
 /*
+const array1 = [1, 2, 3, 4];
+
 // 0 + 1 + 2 + 3 + 4
 const initialValue = 0;
 const sumWithInitial = array1.reduce(
@@ -14,3 +16,52 @@ var newSum = array1.forEach((num) => sum += num)
 console.log(sum);
 */
 
+var emps = [
+    {
+        name: 'Joe Schmoe',
+        yearsExperience: 5,
+        department: 'IT'
+    },
+    {
+        name: 'Sally Sallerson',
+        yearsExperience: 15,
+        department: 'Engineering'
+    },
+    {
+        name: 'Bill Billson',
+        yearsExperience: 5,
+        department: 'Engineering'
+    },
+    {
+        name: 'Jane Janet',
+        yearsExperience: 15,
+        department: 'Management'
+    },
+    {
+        name: 'Bob Hope',
+        yearsExperience: 9,
+        department: 'IT'
+    }
+];
+
+
+
+
+const sumYearsExpiriance = emps.reduce((accumulator, correctValue) => {
+    return accumulator + correctValue.yearsExperience
+}, 0)
+
+console.log('Sum years expiriance is:', sumYearsExpiriance)
+
+const deptCollectiveExp = emps.reduce((accumulator, correctValue) => {
+    const strCurrectKeyValue = correctValue.department
+    console.log(strCurrectKeyValue)
+    if (accumulator.hasOwnProperty(strCurrectKeyValue)) {
+        accumulator[strCurrectKeyValue] += correctValue.yearsExperience
+    }else{
+        accumulator[strCurrectKeyValue] = correctValue.yearsExperience
+    }
+    return accumulator
+}, {})
+
+console.log('The map of dep and there collective years :', deptCollectiveExp)
